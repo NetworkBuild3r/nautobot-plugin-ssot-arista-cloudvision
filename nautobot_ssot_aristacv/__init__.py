188aa6df1f5a211beac1fa4b0e4a17f9c96acd26
nobod <brian.nelson@ahead.com>
2023-10-18 11:32:47 -0600

    moved 2.999

--- nautobot_ssot_aristacv/__init__.py
+++ nautobot_ssot_aristacv/__init__.py
@@ -24,7 +24,7 @@ class NautobotSSOTAristaCVConfig(PluginConfig):
     base_url = "nautobot-sot_aristacv"
     required_settings = []
     min_version = "1.4.0"
-    max_version = "latest"
+    max_version = "2.9999"
     default_settings = {
         "cvp_host": os.getenv("NAUTOBOT_ARISTACV_HOST"),
         "cvp_user": os.getenv("NAUTOBOT_ARISTACV_USERNAME"),
